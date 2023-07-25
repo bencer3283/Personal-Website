4b2af45191a887912c3f232dc36f65e17c710ae4
ChengPoSheng <bencer@outlook.com>
2023-07-25 13:43:29 +0800

    fix card size

--- src/components/desktopNav.js
+++ src/components/desktopNav.js
@@ -2,61 +2,65 @@ import * as React from 'react';
 import {
     Button,
     Card,
+    CardHeader,
     CardBody,
     CardFooter,
     HStack,
-    Stack,
     Heading,
-    Text
+    Text,
 } from '@chakra-ui/react';
-import { StaticImage } from 'gatsby-plugin-image';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 
 function DesktopNav({ directory }) {
-    const data = useStaticQuery(graphql`query {
-        allMarkdownRemark {
+    const data = useStaticQuery(graphql`query fileSort {
+        allFile(sort: {modifiedTime: DESC}, filter: {extension: {eq: "md"}}) {
           nodes {
-            parent {
-              ... on File {
-                name
-                relativeDirectory
+            childrenMarkdownRemark {
+              frontmatter {
+                title
+                meta_description
               }
             }
-            frontmatter {
-              title
-              meta_description
-            }
+            name
+            relativeDirectory
           }
         }
       }
     `);
-    return <HStack maxH='sm' bg='#9EC972' overflowX='auto'>
+    return <HStack bg='#9EC972' overflowX='auto'>
         {
-            data.allMarkdownRemark.nodes.map((md) => {
-                if(md.parent.relativeDirectory === directory) {
+            data.allFile.nodes.map((file) => {
+                if(file.relativeDirectory === directory) {
                     return(
-                        <Card w={{base: '40%', md: 'sm'}} direction='row' m='4' flexShrink='0'>
-                            <Stack>
-                                <CardBody>
-                                    <Heading size='md'>
-                                        {md.frontmatter.title}
-                                    </Heading>
-                                    <Text>
-                                        {md.frontmatter.meta_description}
-                                    </Text>
-                                </CardBody>
-                                <CardFooter>
-                                    <Link to={`/${md.parent.relativeDirectory}/${md.parent.name}`}>
-                                        <Button variant='outline'>
-                                            Learn more
-                                        </Button>
-                                    </Link>
-                                    
-                                </CardFooter>
-                            </Stack>
+                        <Card w={{ base: '40vw', md: '30vw' }} h={{base: '25em', md: '20em'}} direction='column' m='3' flexShrink='0'>
+                            
+                            <CardHeader>
+                                <Heading size='md' noOfLines={{base: 3, md: 2}}>
+                                    {file.childrenMarkdownRemark[0].frontmatter.title}
+                                </Heading>
+                            </CardHeader>
+                            
+                            <CardBody>
+
+                                <Text noOfLines={4}>
+                                    {file.childrenMarkdownRemark[0].frontmatter.meta_description}
+                                </Text>
+                            </CardBody>
+                            
+                            <CardFooter>
+                                <Link to={`/${file.relativeDirectory}/${file.name}`}>
+                                    <Button variant='outline'>
+                                        Learn more
+                                    </Button>
+                                </Link>
+
+                            </CardFooter>
+                            
                         </Card>
                     )
+                    
                 }
+                else return null;
             })
         }
     </HStack>;
