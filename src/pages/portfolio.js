781dfe8627b85870809f93b358bf0ce762267ae2
ChengPoSheng <bencer@outlook.com>
2023-11-05 16:37:45 +0200

    two teleshif layout

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import Layout from '../components/Layout';
 import { SEO } from '../components/seo';
-import { Container, Slider, SliderFilledTrack, SliderTrack, SliderThumb, Center, Avatar, Box } from '@chakra-ui/react';
+import { Container, Slider, SliderFilledTrack, SliderTrack, SliderThumb, Center, Avatar, Box, HStack } from '@chakra-ui/react';
 
 const PortfolioPage = () => {
     const [oneSize, setOneSize] = useState('15vh');
@@ -14,29 +14,57 @@ const PortfolioPage = () => {
             <Container w={'50vw'} h={oneSize} bg={'blue.300'} rotate={'60deg'}>
                 {oneSize.toString()}
             </Container>
-            <Center p={'2vh'}>
-                <div style={{ transform: `rotate(0deg) translateY(0vh)` }}>
-                    <TeleshiftArm armController={setOneSize} direction={'vertical'} />
-                </div>
-                <div style={{ transform: `rotate(60deg) translate3d(8vh, -4vh, 0)` }}>
-                    <TeleshiftArm armController={setOneSize} direction={'vertical'} />
-                </div>
-                <div style={{ transform: `rotate(30deg) translate3d(4vh, 15vh, 0)` }}>
-                    <TeleshiftArm armController={setOneSize} direction={'horizontal'} />
-                </div>
-                <div style={{ transform: `rotate(0deg) translate3d(-24vh, 20vh, 0)` }}>
-                    <TeleshiftArm armController={setOneSize} direction={'vertical'} isReversed={true} />
-                </div>
-                <div style={{ transform: `rotate(-60deg) translate3d(-21vh, -27vh, 0)` }}>
-                    <TeleshiftArm armController={setOneSize} direction={'vertical'} />
-                </div>
-                <div style={{ transform: `rotate(-30deg) translate3d(-47vh, -9vh, 0)` }}>
-                    <TeleshiftArm armController={setOneSize} direction={'horizontal'} isReversed={true} />
-                </div>
-                <Box borderRadius={'50%'} w={'10vh'} h={'10vh'} bg={'blue.300'} style={{
-                    transform: `translate3d(-51vh, 10vh, 0)`
-                }}/>
-            </Center>
+            <HStack m={'20vh'}>
+                <Center p={'2vh'}>
+                    <div style={{ transform: `rotate(0deg) translateY(0vh)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} />
+                    </div>
+                    <div style={{ transform: `rotate(60deg) translate3d(8vh, -4vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} />
+                    </div>
+                    <div style={{ transform: `rotate(30deg) translate3d(4vh, 15vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'horizontal'} />
+                    </div>
+                    <div style={{ transform: `rotate(0deg) translate3d(-24vh, 20vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} isReversed={true} />
+                    </div>
+                    <div style={{ transform: `rotate(-60deg) translate3d(-21vh, -27vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} />
+                    </div>
+                    <div style={{ transform: `rotate(-30deg) translate3d(-47vh, -9vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'horizontal'} isReversed={true} />
+                    </div>
+                    <Box borderRadius={'50%'} w={'10vh'} h={'10vh'} bg={'blue.300'} style={{
+                        transform: `translate3d(-51vh, 10vh, 0)`
+                    }} />
+                </Center>
+                <Center p={'2vh'}>
+                    <div style={{ transform: `rotate(0deg) translateY(0vh)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} />
+                    </div>
+                    <div style={{ transform: `rotate(60deg) translate3d(8vh, -4vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} />
+                    </div>
+                    <div style={{ transform: `rotate(30deg) translate3d(4vh, 15vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'horizontal'} />
+                    </div>
+                    <div style={{ transform: `rotate(0deg) translate3d(-24vh, 20vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} isReversed={true} />
+                    </div>
+                    <div style={{ transform: `rotate(-60deg) translate3d(-21vh, -27vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'vertical'} />
+                    </div>
+                    <div style={{ transform: `rotate(-30deg) translate3d(-47vh, -9vh, 0)` }}>
+                        <TeleshiftArm armController={setOneSize} direction={'horizontal'} isReversed={true} />
+                    </div>
+                    <Box borderRadius={'50%'} w={'10vh'} h={'10vh'} bg={'blue.300'} style={{
+                        transform: `translate3d(-51vh, 10vh, 0)`
+                    }} />
+                </Center>
+            </HStack>
+            <Container w={'50vw'} h={oneSize} bg={'blue.300'} rotate={'60deg'}>
+                {oneSize.toString()}
+            </Container>
 
         </Layout>
     )
