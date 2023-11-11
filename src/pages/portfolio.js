c8cba343afbe9909aec4dbc8173d394b26cf0de5
ChengPoSheng <bencer@outlook.com>
2023-11-11 16:46:13 +0200

    three modes of interaction

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,18 +2,48 @@ import * as React from 'react';
 import { useState } from 'react';
 import Layout from '../components/Layout';
 import { SEO } from '../components/seo';
-import { Container, Slider, SliderFilledTrack, SliderTrack, SliderThumb, Center, useControllableState, Box, HStack } from '@chakra-ui/react';
+import { Text, Slider, SliderFilledTrack, SliderTrack, SliderThumb, Center, Box, HStack, Tab, Tabs, TabList, TabPanel, TabPanels, Button } from '@chakra-ui/react';
 
-let initArm = Array(12).fill(14);
-
+let initArm = Array(12).fill(16);
 
 const PortfolioPage = () => {
     const [armLength, setArmLength] = useState(initArm);
+    const [armMode, setArmMode] = useState(0);
 
     function handleArmInteraction(newVal, id, length) {
         const newLength = length.map((e, i) => {
-            if (i === id || i === id + 6) {
-                return newVal
+            if (armMode === 0) {
+                if (i === id || i === id + 6) {
+                    return newVal
+                }
+                else {
+                    return e
+                }
+            }
+            else if (armMode === 1) {
+                if (i === id || i === id + 6 || i === id - 6) {
+                    return newVal
+                }
+                else {
+                    return e
+                }
+            }
+            else {
+                if (i === id) {
+                    return newVal
+                }
+                else {
+                    return e
+                }
+            }
+        })
+        setArmLength(newLength);
+    }
+
+    function memorize() {
+        const newLength = armLength.map((e, i) => {
+            if (i >= 6) {
+                return armLength[i-6]
             }
             else {
                 return e
@@ -43,7 +73,7 @@ const PortfolioPage = () => {
             <SliderTrack>
                 <SliderFilledTrack />
             </SliderTrack>
-            <SliderThumb />
+            <SliderThumb bgColor={'blue.100'}/>
         </Slider>;
     }
 
@@ -71,7 +101,15 @@ const PortfolioPage = () => {
                     </div>
                     <Box borderRadius={'50%'} w={'10vh'} h={'10vh'} bg={'blue.300'} style={{
                         transform: `translate3d(-51vh, 10vh, 0)`
-                    }} />
+                    }} textAlign={'center'}> <div style={{
+                        position: `absolute`,
+                        top: `50%`,
+                        left: `50%`,
+                        transform: `translate(-50%, -50%)`
+                    }}>
+                            {armMode === 0 ? 'Master' : ' '}
+                            {armMode === 2 ? 'Memory' : ' '}
+                        </div></Box>
                 </Center>
                 <Center p={'2vh'}>
                     <div style={{ transform: `rotate(0deg) translateY(0vh)` }}>
@@ -94,10 +132,41 @@ const PortfolioPage = () => {
                     </div>
                     <Box borderRadius={'50%'} w={'10vh'} h={'10vh'} bg={'blue.300'} style={{
                         transform: `translate3d(-51vh, 10vh, 0)`
-                    }} />
+                    }} >
+                        <div style={{
+                            position: `absolute`,
+                            top: `50%`,
+                            left: `50%`,
+                            transform: `translate(-50%, -50%)`
+                        }}>
+                            {armMode === 0 ? 'Slave' : ' '}
+                        </div>
+                    </Box>
                 </Center>
             </HStack>
+            <Tabs variant='soft-rounded' colorScheme='blue' onChange={(mode) => {
+                setArmMode(mode);
+            }}>
 
+                <TabPanels>
+                    <TabPanel>
+                        <p>Each arm of slave TeleSHift is programmed to follow the position of master TeleSHift, but not vice versa.</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Each arm of both TeleSHifts is synced with the respective arm on the other TeleSHift.</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <Button onClick={memorize} mb={'12px'}>Restore</Button>
+                        <Text>TeleSHift can be commanded to restore a pre-defined shape in its memory.</Text>
+                        
+                    </TabPanel>
+                </TabPanels>
+                <TabList>
+                    <Tab>Master & Slave</Tab>
+                    <Tab>Mutual Sync</Tab>
+                    <Tab>Shape Memory</Tab>
+                </TabList>            
+            </Tabs>
         </Layout>
     )
 }
