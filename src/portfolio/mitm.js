5b59cdeff0effcb8b3b2213890b0008032305e66
Po-Sheng Cheng <bencer@outlook.com>
2024-12-24 21:08:49 -0500

    update mitm live demo link

--- src/portfolio/mitm.js
+++ src/portfolio/mitm.js
@@ -32,7 +32,7 @@ export default function MITMSection() {
                     <TabPanel>
                         <Card w={{base: '2xs',md: '5xl'}} direction={{base: 'column', md: 'row'}} variant={'unstyle'}>
                             <CardBody>
-                                <div style={{ padding: `56.25% 0 0 0`, position: `relative` }}><iframe src="https://player.vimeo.com/video/1041628137?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{ position: `absolute`, top: `0`, left: `0`, width: `100%`, height: `100%` }} title="mitm-live-demo"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                                <div style={{ padding: `56.25% 0 0 0`, position: `relative` }}><iframe src="https://player.vimeo.com/video/1042066539?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{ position: `absolute`, top: `0`, left: `0`, width: `100%`, height: `100%` }} title="mitm-live-demo"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                             </CardBody> 
                             <CardBody>
                                 <OrderedList textAlign={'start'} m={4}>
